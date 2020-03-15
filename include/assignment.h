af4f62bebc45d69f9eced39ee648ecfbf8dcb8ab
Eric Andrade <vloxflox@gmail.com>
2020-03-14 21:21:44 -0300

    attributes generate  derivates attributes (needs testing)

--- include/assignment.h
+++ include/assignment.h
@@ -3,15 +3,25 @@
 
 #include <list>
 #include "task.h"
+#include <vector>
 
 class Assignment{
 public:
+
 	std::list<Task> task_list;
+
+	std::vector<Task> pending_tasks;	
+	std::vector<Assignment*> next_states;
+
+
 	int delay_score;
 
-	void update_score();
+	void calc_score();
 	void add_task(Task);
 
+	void make_next_states();
+
+	Assignment();
 };
 
 
--- src/assignment.cpp
+++ src/assignment.cpp
@@ -1,6 +1,9 @@
 #include "assignment.h"
+#include <vector>
 
-void Assignment::update_score(){
+Assignment::Assignment(){}
+
+void Assignment::calc_score(){
 	delay_score = 0;
 	for (std::list<Task>::iterator it=task_list.begin(); it != task_list.end(); ++it){
 		delay_score += it->deadline - it->end_time;
@@ -20,3 +23,57 @@ void Assignment::add_task(Task task){
 	}
 
 }
+
+void Assignment::make_next_states(){
+	//cria uma cópia do vetor de tarefas pendentes (para manter intacto o atual)
+	//deleta o copiado de trás pra frente e cria Assignments filhos atualizando suas tarefas e pendências
+	std::vector<Task> pending_tasks_cpy;
+	pending_tasks_cpy = this->pending_tasks;
+
+	//Lista de controle das tarefas a serem adicionadas
+	std::vector<Task> pending_tasks_update;
+
+	//numero de estados a ser gerado na próxima iteração
+	int num_next = pending_tasks_cpy.size();
+
+	//Auxiliar para criação de tarefas
+	Assignment* created_assig;
+
+	//nº do nó filho, varia de 0 a (n-1)
+	//cada um adiciona recebe um elemento da lista pendência à lista de ativos
+	int insertion_index;
+
+	//inserts backwards (preserves Task order without using searchs)
+	//also creates more predictable trees
+	insertion_index = pending_tasks_cpy.size() - 1;
+
+	for(int i=0;i < num_next;i++){
+		//resets the pending task list
+		pending_tasks_update = this->pending_tasks;
+
+		//creates a new empty task object
+		created_assig = new Assignment();
+
+		//copies current tasks
+		created_assig->task_list = this->task_list;
+
+		//adds one task from the pending list
+		created_assig->add_task(pending_tasks_cpy.back());
+		pending_tasks_cpy.pop_back();
+
+		//using tree order, it's possible to know which pending task should be missing on the new assignment
+		//the same vector that was used to create the new node now is removed from it's pending list
+		pending_tasks_update.erase(pending_tasks_update.begin() + insertion_index);
+
+		//sets the new list to the new assignment
+		created_assig->pending_tasks = pending_tasks_update;
+
+
+		//inserts the created assignment backwards (to preserve the tree order)
+		next_states[insertion_index] = created_assig;
+		insertion_index--;
+
+	}
+
+}
+
